8a1f00c7d62b4171036ea4024793ae179e480330
Ryan Van Ess <fallen.rve@gmail.com>
2016-12-19 21:48:40 -0600

    adds event buttons, creates click handler for add ticket

--- app/index.js
+++ app/index.js
@@ -3,25 +3,35 @@ import manageTickets from './modules/manageTickets';
 (function IIFE() {
     'use strict';
 
+    function updateTicketList() {
+        get.byId("app").innerHTML =
+            "<ul>" +
+                manageTickets.getAllTickets().map(function list(c, i) {
+                    return `<li key=${i}>${c}</li>`;
+                }).join("") +
+            "</ul>";
+    }
+
+
     manageTickets.addTicket("thing");
     manageTickets.addTicket("stuff");
+    updateTicketList();
 
-    console.log(`Ticket Count: ${manageTickets.ticketCount()}`);
-    get.byId("app").innerHTML =
-        "<ul>" +
-            manageTickets.getAllTickets().map(function list(c, i) {
-                return `<li key=${i}>${c}</li>`;
-            }).join("") +
-        "</ul>";
+    [].forEach.call(get.byClass('action'), function(e, i, a) {
+        e.addEventListener('click', function handleClick(e) {
+            switch (e.target.dataset.action) {
+                case 'add':
+                    manageTickets.addTicket('another ticket');
+                    break;
+                case 'clear':
+                    console.log('clear');
+                    break;
+                default:
+                    console.log('no case');
+                    break;
+            }
+            updateTicketList();
+        });
+    });
 
-    console.log(`Primary Ticket: ${manageTickets.primaryTicket}`);
-
-    console.log("All Tickets:");
-    console.log(manageTickets.getAllTickets());
-
-    manageTickets.clearTickets();
-    console.log("Cleared Tickets:");
-    console.log(manageTickets.getAllTickets());
-
-    console.log(get.bySelector(".classes"));
 }());
